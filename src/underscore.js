262b418dc564927f86af4dbee0152ed6b47959d0
Perlou <perloukevin@gmail.com>
2017-08-21 21:03:15 +0800

    project init

---
+++ src/underscore.js
@@ -0,0 +1,20 @@
+/**
+ * underscore.js note
+ * @author Perlou(perloukevin@gmail.com)
+ */
+
+(function () {
+
+    // var root = typeof self == 'object' && self.self
+    var root = {};
+
+    var previousUnderscore = root._;
+
+    var ArrayProto = Array.prototype,
+        ObjectProto = Object.prototype;
+
+    console.log(Symbol)
+
+    // var SymbolProto = typeof Symbol !== 'undefined' ? Symbol
+
+})()
